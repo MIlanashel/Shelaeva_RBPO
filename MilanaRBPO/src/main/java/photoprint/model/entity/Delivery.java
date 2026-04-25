bc92c87c9dae23ee1ec18466352ba49bf3511498
MIlanashel <milanashelaevasans@gmail.com>
2026-04-25 15:38:42 +0300

    Update Delivery.java

--- MilanaRBPO/src/main/java/photoprint/model/entity/Delivery.java
+++ MilanaRBPO/src/main/java/photoprint/model/entity/Delivery.java
@@ -1,6 +1,7 @@
 package photoprint.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 
 @Entity
@@ -20,37 +21,18 @@ public class Delivery {
     @JsonIgnore
     private Order order;
 
-    public Delivery(String address, Order order){
-        this.order = order;
+    public Delivery() {}
+
+    public Delivery(String address, Order order) {
         this.address = address;
-    }
-
-    public Delivery(){
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
         this.order = order;
     }
+
+    public Long getId() { return id; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public Order getOrder() { return order; }
+    public void setOrder(Order order) { this.order = order; }
 }
-
